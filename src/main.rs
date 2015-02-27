cfd718a11afc4d819fd27d1855b869b16ec9ddf0
David Lamont <del6597@rit.edu>
2015-02-26 20:38:37 -0800

    Accepts connections on port 6667.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::net::{TcpStream, TcpListener};
+use std::thread;
+
+fn handle_client(stream: TcpStream) {
+	println!("Someone connected!");
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting Iron IRCd");
+
+	let listener = TcpListener::bind("127.0.0.1:6667").unwrap();
+
+	for stream in listener.incoming() {
+		match stream {
+			Ok(stream) => {
+				thread::spawn(move || {
+					handle_client(stream)
+				});
+			}
+			Err(e) => { /* Connection Failed */ }
+		}
+	}
 }
